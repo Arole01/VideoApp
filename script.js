0301eecfb2fec3d1f47753846e655f9ca6ceef79
Arole01 <ajosedavidayobami@gmail.com>
2025-11-01 00:19:12 +0000

    scriptadded

--- script.js
+++ script.js
@@ -11,3 +11,34 @@ async function initCamera() {
     preview.srcObject = stream;
     return stream;
 }
+
+startBtn.onclick = async () => {
+    const stream = await initCamera();
+    recordedChunks=[];
+    mediaRecorder = new
+    MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = e => {
+        if(e.data.size > 0)
+            recordedChunks.push(e.data);
+    }
+
+    mediaRecorder.onstop = () => {
+        const blob = newBlob(recordedChunks,{type:'video/webm'});
+        const url = URL.createObjectURL(blob);
+        downloadLink.href = url;
+        downloadLink.download = 'recording.webm';
+        downloadLink.style.display = 'inline-block';
+    };
+
+    mediaRecorder.start()
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    downloadLink.style.display = 'none';
+};
+
+    stopBtn.onclick = () => {
+        mediaRecorder.stop();
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    }
